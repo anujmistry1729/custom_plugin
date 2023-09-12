3c34d48afad241462856b7354a155d002a6824b0
anujmistry1729 <anuj.mistry24@gmail.com>
2023-09-12 23:46:33 +0530

    update cell view

--- imageViewer.js
+++ imageViewer.js
@@ -1,6 +1,5 @@
-//creating a template 
-const templateCell_$PLUGIN_ID = document.createElement("template");
-//adding styles and tags inside the template tags innerHTML
+var templateCell_$PLUGIN_ID = document.createElement('template');
+
 templateCell_$PLUGIN_ID.innerHTML = `
     <style>
         #container-image-url{
@@ -12,101 +11,131 @@ templateCell_$PLUGIN_ID.innerHTML = `
     <div id="container">
         <p id="container-image-url"></p>
     </div>
-`;
-
-class OuterbasePluginCell_$PLUGIN_ID extends HTMLElement{
-//smiliar to previours editor view class constructor except the child 
-    //that is getting appended to the nodoe
-    constructor() {
-//to use HTMLElemement Properties (Specifically shadowDOM)
-      super();
-  
-      this.shadow = this.attachShadow({ mode: "open" });
-      //attach our Modal UI to shadow dom
-        this.shadow.appendChild(templateCell_$PLUGIN_ID.cloneNode(true));
-// //initalize config class for getting values of the cell can be commented and it will still work
-//         this.config = new OuterbasePluginConfig_$PLUGIN_ID(
-//           JSON.parse(this.getAttribute("configuration"))
-//         );
-    }
-  
-    connectedCallback() {
-      const containerImageUrl = this.shadow.getElementById("container-image-url");
-//assign value as innerHTML or as innerTextContent
-      containerImageUrl.innerHTML = this.getAttribute("cellValue");
-  
-      if (containerImageUrl) {
-//IMPORTANT on click event trigger a callCustomEvent property
-        containerImageUrl.addEventListener("click", () => {
-          this.callCustomEvent({
-            action: "onedit",
-            value: containerImageUrl.innerText,
-          });
-        });
-      }
-    }
-//IMPORTANT property to create the custom event and dispatch it.
-    callCustomEvent(data) {
-      const event = new CustomEvent("custom-change", {
-        detail: data,
-        bubbles: true,
-        composed: true,
-      });
-  
-      this.dispatchEvent(event);
-    }
-  }
-
-
-const templateEditor_$PLUGIN_ID= document.createElement("template");
-templateEditor_$PLUGIN_ID.innerHTML = `
-<div class="modal-overlay">
-    <div class="modal">
-        <img id="modal-image" alt="Image" class="modal-image">
-        <input type="url" id="url-input" class="url-input" placeholder="Don't tell anyone its AI generated ;)">
-        <button id="save-button"   class="save-button">Save URL</button>
-    </div>
-</div>
 `
 
-class OuterbasePluginEditor_$PLUGIN_ID extends HTMLElement {
-    constructor() {
-      //to use HTMLElemement Properties (Specifically shadowDOM)
-      super();
-  
-      this.shadow = this.attachShadow({ mode: "open" });
-      //attach our Modal UI to shadow dom
-      this.shadow.appendChild(templateEditor_$PLUGIN_ID.content.cloneNode(true));
-      //initalize config class for getting values of the cell cam be removed 
-    //   this.config = new OuterbasePluginConfig_$PLUGIN_ID(
-    //     JSON.parse(this.getAttribute("configuration")),
-    //   );
-    }
-  
-    connectedCallback() {
-      var modalImage = this.shadow.getElementById("modal-image");
-      var urlInputEl = this.shadow.getElementById("url-input");
-      //add source and values to image and input tag if we get the element
-      if (modalImage && urlInputEl) {
-        modalImage.src = this.getAttribute("cellValue");
-        urlInputEl.value = this.getAttribute("cellValue");
-      }
-    }
+var templateEditor_$PLUGIN_ID = document.createElement('template');
+templateEditor_$PLUGIN_ID.innerHTML = `
+<style>
+.modal{
+    display: block;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0, 0.4);
+    font-family: 'Inter', 'sans-serif';
+    font-size: 16px;
   }
   
-  class OuterbasePluginConfig_$PLUGIN_ID {
-    constructor(object) {}
-  
-    connectedCallback() {}
+  .modal-content{
+    height: 80%;
+    max-width: 520px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
   }
+  
+  .modal-content > input{
+    width: 50%;
+    padding: 8px 16px;
+    border: none;
+    border-radius: .5rem;
+  }
+  
+  .modal-content >  img{
+    width: 100%;
+    height: 80%;
+    object-fit: contain;
+    border-radius: 1rem;
+  }
+  
+  .modal-content > button{
+    background: linear-gradient(rgb(248, 248, 248) 0%, rgb(254, 254, 254) 100%);
+      box-shadow: rgb(176, 176, 176) 0px -1px 1px 1px inset, rgb(252, 252, 252) 0px 0px 2px 0px inset;
+    padding: 6px 16px;
+    border-radius: .5rem;
+    border: none;
+    cursor: pointer;
+  }
+  
+</style>
 
-  //IMPORTANT TO LET THE DOCUMENT KNOW ABOUT CUSTOMELEMENTS 
-  window.customElements.define(
-    "outerbase-plugin-cell-$PLUGIN_ID",
-    OuterbasePluginCell_$PLUGIN_ID
-  );
-  window.customElements.define(
-    "outerbase-plugin-editor-$PLUGIN_ID",
-    OuterbasePluginEditor_$PLUGIN_ID
-  );
-  
+<!-- Modal -->
+<div id="myModal" class="modal">
+
+<!--    Content  -->
+<div class="modal-content"> 
+
+    <img id="preview-image" alt="image">
+    <input id="image-url" type="url" name="url" id="input-url" placeholder="image url">
+    
+    <button id="update-url-btn" >Update</button>
+
+  </div>
+</div>
+
+`
+
+
+//empty as we do not require a config 
+class OuterbasePluginConfig_$PLUGIN_ID{
+    constructor(object){
+
+    }
+}
+
+//PluginCell
+class OuterbasePluginCell_$PLUGIN_ID extends HTMLElement {
+
+    static get ObservedAttributes(){
+        return privileges;
+    };
+
+    //initialize plugin config with empty values
+    config = new OuterbasePluginConfig_$PLUGIN_ID({});
+
+    constructor(){
+        super();
+
+        this.shadow = this.attachShadow({mode: 'open'});
+        this.shadow.appendChild(templateCell_$PLUGIN_ID.content.cloneNode(true));
+    };
+
+    connectedCallback(){
+        
+        this.config = new OuterbasePluginConfig_$PLUGIN_ID(
+            JSON.parse(this.getAttribute('configuration'))
+            );
+        
+        this.shadow.querySelector('#container-image-url').innerHTML = this.getAttribute('cellValue');
+
+        var containerImageUrl = this.shadow.getElementById("container-image-url");
+
+        if(containerImageUrl){
+            containerImageUrl.addEventListener('click',()=>{
+
+                this.callCustomEvent({
+                    action: 'onclick',
+                    value: containerImageUrl.innerText,
+                })
+            })
+        }
+
+    }
+
+    callCustomEvent(data){
+        const event = new CustomEvent('custom-change', {
+            detail: data,
+            bubbles: true,
+            composed: true,
+        });
+
+        this.dispatchEvent(event);
+    }
+}
+
+
+window.customElements.define('outerbase-plugin-cell-$PLUGIN_ID', OuterbasePluginCell_$PLUGIN_ID);
