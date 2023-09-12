20e0e9c45bf82b4dedb28e26d3bedba6cac0a747
anujmistry1729 <anuj.mistry24@gmail.com>
2023-09-12 11:14:50 +0530

    add config class and define custom element to window

--- imageViewer.js
+++ imageViewer.js
@@ -15,47 +15,47 @@ templateCell_$PLUGIN_ID.innerHTML = `
 `;
 
 class OuterbasePluginCell_$PLUGIN_ID extends HTMLElement{
-    //smiliar to previours editor view class constructor except the child 
+//smiliar to previours editor view class constructor except the child 
     //that is getting appended to the nodoe
     constructor() {
-        //to use HTMLElemement Properties (Specifically shadowDOM)
-        super();
-        
-        this.shadow = this.attachShadow({ mode: "open" });
-        //attach our Modal UI to shadow dom
+//to use HTMLElemement Properties (Specifically shadowDOM)
+      super();
+  
+      this.shadow = this.attachShadow({ mode: "open" });
+      //attach our Modal UI to shadow dom
         this.shadow.appendChild(templateCell_$PLUGIN_ID.cloneNode(true));
-        //initalize config class for getting values of the cell
-        this.config = new OuterbasePluginConfig_$PLUGIN_ID(
-          JSON.parse(this.getAttribute("configuration"))
-        );
-      }
-
+// //initalize config class for getting values of the cell can be commented and it will still work
+//         this.config = new OuterbasePluginConfig_$PLUGIN_ID(
+//           JSON.parse(this.getAttribute("configuration"))
+//         );
+    }
+  
     connectedCallback() {
-        const containerImageUrl = this.shadow.getElementById("container-image-url");
-        //assign value as innerHTML or as innerTextContent
-        containerImageUrl.innerHTML = this.getAttribute("cellValue");
-    
-        if (containerImageUrl) {
-        //IMPORTANT on click event trigger a callCustomEvent property
+      const containerImageUrl = this.shadow.getElementById("container-image-url");
+//assign value as innerHTML or as innerTextContent
+      containerImageUrl.innerHTML = this.getAttribute("cellValue");
+  
+      if (containerImageUrl) {
+//IMPORTANT on click event trigger a callCustomEvent property
         containerImageUrl.addEventListener("click", () => {
-                this.callCustomEvent({
-                action: "onedit",
-                value: containerImageUrl.innerText,
-            });
+          this.callCustomEvent({
+            action: "onedit",
+            value: containerImageUrl.innerText,
+          });
         });
-        }
       }
-    //IMPORTANT property to create the custom event and dispatch it.
-  callCustomEvent(data) {
-    const event = new CustomEvent("custom-change", {
-      detail: data,
-      bubbles: true,
-      composed: true,
-    });
-
-    this.dispatchEvent(event);
+    }
+//IMPORTANT property to create the custom event and dispatch it.
+    callCustomEvent(data) {
+      const event = new CustomEvent("custom-change", {
+        detail: data,
+        bubbles: true,
+        composed: true,
+      });
+  
+      this.dispatchEvent(event);
+    }
   }
-}
 
 
 const templateEditor_$PLUGIN_ID= document.createElement("template");
@@ -77,10 +77,10 @@ class OuterbasePluginEditor_$PLUGIN_ID extends HTMLElement {
       this.shadow = this.attachShadow({ mode: "open" });
       //attach our Modal UI to shadow dom
       this.shadow.appendChild(templateEditor_$PLUGIN_ID.content.cloneNode(true));
-      //initalize config class for getting values of the cell
-      this.config = new OuterbasePluginConfig_$PLUGIN_ID(
-        JSON.parse(this.getAttribute("configuration")),
-      );
+      //initalize config class for getting values of the cell cam be removed 
+    //   this.config = new OuterbasePluginConfig_$PLUGIN_ID(
+    //     JSON.parse(this.getAttribute("configuration")),
+    //   );
     }
   
     connectedCallback() {
@@ -94,4 +94,19 @@ class OuterbasePluginEditor_$PLUGIN_ID extends HTMLElement {
     }
   }
   
+  class OuterbasePluginConfig_$PLUGIN_ID {
+    constructor(object) {}
+  
+    connectedCallback() {}
+  }
+
+  //IMPORTANT TO LET THE DOCUMENT KNOW ABOUT CUSTOMELEMENTS 
+  window.customElements.define(
+    "outerbase-plugin-cell-$PLUGIN_ID",
+    OuterbasePluginCell_$PLUGIN_ID
+  );
+  window.customElements.define(
+    "outerbase-plugin-editor-$PLUGIN_ID",
+    OuterbasePluginEditor_$PLUGIN_ID
+  );
   
