2cc69e7d3811d091d4a2993ab406f37893ec58c8
anujmistry1729 <anuj.mistry24@gmail.com>
2023-09-12 23:47:16 +0530

    add editor modal and interaction

--- imageViewer.js
+++ imageViewer.js
@@ -1,4 +1,4 @@
-var templateCell_$PLUGIN_ID = document.createElement('template');
+var templateCell_$PLUGIN_ID = document.createElement("template");
 
 templateCell_$PLUGIN_ID.innerHTML = `
     <style>
@@ -11,24 +11,21 @@ templateCell_$PLUGIN_ID.innerHTML = `
     <div id="container">
         <p id="container-image-url"></p>
     </div>
-`
+`;
 
-var templateEditor_$PLUGIN_ID = document.createElement('template');
+var templateEditor_$PLUGIN_ID = document.createElement("template");
 templateEditor_$PLUGIN_ID.innerHTML = `
 <style>
 .modal{
     display: block;
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
+    position: absolute;
     background-color: rgba(0,0,0, 0.4);
     font-family: 'Inter', 'sans-serif';
     font-size: 16px;
-  }
-  
-  .modal-content{
+    transform: translate(50%,50%);
+}
+
+.modal-content{
     height: 80%;
     max-width: 520px;
     margin: 0 auto;
@@ -36,27 +33,28 @@ templateEditor_$PLUGIN_ID.innerHTML = `
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-  }
+}
   
   .modal-content > input{
-    width: 50%;
-    padding: 8px 16px;
-    border: none;
-    border-radius: .5rem;
-  }
-  
-  .modal-content >  img{
-    width: 100%;
-    height: 80%;
-    object-fit: contain;
-    border-radius: 1rem;
-  }
-  
-  .modal-content > button{
-    background: linear-gradient(rgb(248, 248, 248) 0%, rgb(254, 254, 254) 100%);
-      box-shadow: rgb(176, 176, 176) 0px -1px 1px 1px inset, rgb(252, 252, 252) 0px 0px 2px 0px inset;
-    padding: 6px 16px;
-    border-radius: .5rem;
+      width: 50%;
+      padding: 8px 16px;
+      border: none;
+      border-radius: .5rem;
+    }
+    
+    .modal-content >  img{
+        width: 100%;
+        height: 80%;
+        object-fit: contain;
+        border-radius: 1rem;
+    }
+    
+    .modal-content > button{
+        color: black;
+        background: linear-gradient(rgb(248, 248, 248) 0%, rgb(254, 254, 254) 100%);
+        box-shadow: rgb(176, 176, 176) 0px -1px 1px 1px inset, rgb(252, 252, 252) 0px 0px 2px 0px inset;
+        padding: 6px 16px;
+        border-radius: .5rem;
     border: none;
     cursor: pointer;
   }
@@ -64,78 +62,148 @@ templateEditor_$PLUGIN_ID.innerHTML = `
 </style>
 
 <!-- Modal -->
-<div id="myModal" class="modal">
+<div id="container" class="modal">
 
 <!--    Content  -->
 <div class="modal-content"> 
 
     <img id="preview-image" alt="image">
-    <input id="image-url" type="url" name="url" id="input-url" placeholder="image url">
+    <input id="input-url" type="url" name="url" placeholder="image url">
     
-    <button id="update-url-btn" >Update</button>
+    <button id="update-url-btn" class="btn btn-primary" >Update</button>
 
   </div>
 </div>
 
-`
+`;
 
+//empty as we do not require a config
+class OuterbasePluginConfig_$PLUGIN_ID {
+  constructor(object) {}
 
-//empty as we do not require a config 
-class OuterbasePluginConfig_$PLUGIN_ID{
-    constructor(object){
+  connectedCallback() {
+    this.loadExternalScript([
+      "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css",
+      "https://code.jquery.com/jquery-3.2.1.slim.min.js",
+      "https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js",
+      "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js",
+    ]);
+  }
 
-    }
+  loadExternalScript(url) {
+    return new Promise((resolve, reject) => {
+      if (url.length) {
+        url.map((singleUrl) => {
+          const script = document.createElement("script");
+          script.src = url;
+
+          script.onload = () => {
+            resolve();
+          };
+
+          script.onerror = () => {
+            reject(new Error(`Failed to load script: ${url}`));
+          };
+
+          document.head.appendChild(script);
+        });
+      }
+      const script = document.createElement("script");
+      script.src = url;
+
+      script.onload = () => {
+        resolve();
+      };
+
+      script.onerror = () => {
+        reject(new Error(`Failed to load script: ${url}`));
+      };
+
+      document.head.appendChild(script);
+    });
+  }
 }
 
 //PluginCell
 class OuterbasePluginCell_$PLUGIN_ID extends HTMLElement {
+  static get ObservedAttributes() {
+    return privileges;
+  }
 
-    static get ObservedAttributes(){
-        return privileges;
-    };
+  //initialize plugin config with empty values
+  config = new OuterbasePluginConfig_$PLUGIN_ID({});
 
-    //initialize plugin config with empty values
-    config = new OuterbasePluginConfig_$PLUGIN_ID({});
+  constructor() {
+    super();
 
-    constructor(){
-        super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.appendChild(templateCell_$PLUGIN_ID.content.cloneNode(true));
+  }
 
-        this.shadow = this.attachShadow({mode: 'open'});
-        this.shadow.appendChild(templateCell_$PLUGIN_ID.content.cloneNode(true));
-    };
+  connectedCallback() {
+    this.config = new OuterbasePluginConfig_$PLUGIN_ID(
+      JSON.parse(this.getAttribute("configuration"))
+    );
 
-    connectedCallback(){
-        
-        this.config = new OuterbasePluginConfig_$PLUGIN_ID(
-            JSON.parse(this.getAttribute('configuration'))
-            );
-        
-        this.shadow.querySelector('#container-image-url').innerHTML = this.getAttribute('cellValue');
+    this.shadow.querySelector("#container-image-url").innerHTML =
+      this.getAttribute("cellValue");
 
-        var containerImageUrl = this.shadow.getElementById("container-image-url");
+    var containerImageUrl = this.shadow.getElementById("container-image-url");
 
-        if(containerImageUrl){
-            containerImageUrl.addEventListener('click',()=>{
-
-                this.callCustomEvent({
-                    action: 'onclick',
-                    value: containerImageUrl.innerText,
-                })
-            })
-        }
-
-    }
-
-    callCustomEvent(data){
-        const event = new CustomEvent('custom-change', {
-            detail: data,
-            bubbles: true,
-            composed: true,
+    if (containerImageUrl) {
+      containerImageUrl.addEventListener("click", () => {
+        this.callCustomEvent({
+          action: "onedit",
+          value: containerImageUrl.innerText,
         });
-
-        this.dispatchEvent(event);
+      });
     }
+  }
+
+  callCustomEvent(data) {
+    const event = new CustomEvent("custom-change", {
+      detail: data,
+      bubbles: true,
+      composed: true,
+    });
+
+    this.dispatchEvent(event);
+  }
 }
 
+class OuterbasePluginEditor_$PLUGIN_ID extends HTMLElement {
+  static get observedAttributes() {
+    return privileges;
+  }
 
-window.customElements.define('outerbase-plugin-cell-$PLUGIN_ID', OuterbasePluginCell_$PLUGIN_ID);
+  constructor() {
+    super();
+
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.appendChild(templateEditor_$PLUGIN_ID.content.cloneNode(true));
+
+    this.config = new OuterbasePluginConfig_$PLUGIN_ID(
+      JSON.parse(this.getAttribute("configuration"))
+    );
+  }
+
+  connectedCallback() {
+    var previewImageEl = this.shadow.getElementById("preview-image");
+    var inputURLEl = this.shadow.getElementById("input-url");
+
+    console.log(previewImageEl, inputURLEl, inputURLEl && inputURLEl);
+    if (previewImageEl && inputURLEl) {
+      previewImageEl.src = this.getAttribute("cellValue");
+      inputURLEl.value = this.getAttribute("cellValue");
+    }
+  }
+}
+
+window.customElements.define(
+  "outerbase-plugin-cell-$PLUGIN_ID",
+  OuterbasePluginCell_$PLUGIN_ID
+);
+window.customElements.define(
+  "outerbase-plugin-editor-$PLUGIN_ID",
+  OuterbasePluginEditor_$PLUGIN_ID
+);
